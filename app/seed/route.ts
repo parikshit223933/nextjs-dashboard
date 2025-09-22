fe261fba1a1ec084bc748ae35a6ba44d7d225ae7
Parikshit Singh <pk223933@gmail.com>
2025-09-22 17:19:34 +0530

    starting chapter 13

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,9 +1,9 @@
-// import bcrypt from 'bcrypt';
-// import postgres from 'postgres';
+import bcrypt from 'bcrypt';
+import postgres from 'postgres';
 // import { invoices, customers, revenue, users } from '../lib/placeholder-data';
-//
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-//
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
 // async function seedUsers() {
 //   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 //   await sql`
@@ -100,18 +100,18 @@
 //
 //   return insertedRevenue;
 // }
-//
-// export async function GET() {
-//   try {
-//     const result = await sql.begin((sql) => [
-//       seedUsers(),
-//       seedCustomers(),
-//       seedInvoices(),
-//       seedRevenue(),
-//     ]);
-//
-//     return Response.json({ message: 'Database seeded successfully' });
-//   } catch (error) {
-//     return Response.json({ error }, { status: 500 });
-//   }
-// }
+
+export async function GET() {
+  try {
+    // const result = await sql.begin((sql) => [
+    //   seedUsers(),
+    //   seedCustomers(),
+    //   seedInvoices(),
+    //   seedRevenue(),
+    // ]);
+
+    return Response.json({ message: 'Database seeded successfully' });
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
